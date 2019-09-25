98740bc84fe2a10a31cabc8615cd66adce4dc7c8
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-25 17:03:45 +0300

    Create class Respondent that extends abstract class Party

--- src/main/java/models/Respondent.java
+++ src/main/java/models/Respondent.java
@@ -1,4 +1,11 @@
 package models;
 
-public class Respondent {
+public class Respondent extends Party {
+    public static final String DATABASE_TYPE = "respondent";
+    public Respondent(String name) {
+        this.name= name;
+        type = DATABASE_TYPE;
+    }
+
+
 }
