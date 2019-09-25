f5935045b3ddea4cdf0f830d76559bf41f0e6149
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-25 17:33:14 +0300

    Test for class interested party

--- src/test/java/models/InterestedPartyTest.java
+++ src/test/java/models/InterestedPartyTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,28 @@ public class InterestedPartyTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void InterestedPartyInstantiatesCorrectly() throws Exception{
+        InterestedParty interestedParty  = setUpInterestedParty();
+        assertTrue( interestedParty instanceof InterestedParty);
+    }
+
+    @Test
+    public void getName() {
+        InterestedParty interestedParty  = setUpInterestedParty();
+        assertEquals("Robert",interestedParty.getName());
+    }
+
+    @Test
+    public void getType() {
+        InterestedParty interestedParty  = setUpInterestedParty();
+        assertEquals("interested party",interestedParty.getType());
+    }
+
+    //helper method
+    public InterestedParty setUpInterestedParty (){
+        return new InterestedParty("Robert");
     }
 }
