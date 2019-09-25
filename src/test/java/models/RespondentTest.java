6fb4d43973bfa037410bdd70b551bafbd90f7113
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-25 17:04:32 +0300

    Test for instansiation of class Respondent

--- src/test/java/models/RespondentTest.java
+++ src/test/java/models/RespondentTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +14,16 @@ public class RespondentTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void RespondentInstantiatesCorrectly() throws Exception{
+        Respondent respondent = setUpRespondent();
+        assertTrue( respondent instanceof Respondent);
+    }
+
+    //helper
+    public Respondent setUpRespondent(){
+        return new Respondent("Daisy");
     }
 }
