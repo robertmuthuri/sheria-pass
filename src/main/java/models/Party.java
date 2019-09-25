14420ad95adee2beeeab7a876ccc1dff9f481819
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-25 17:02:59 +0300

    Add setters and getters

--- src/main/java/models/Party.java
+++ src/main/java/models/Party.java
@@ -1,10 +1,31 @@
 package models;
 
 public  abstract class  Party {
-    private  String name;
-    private String type;
-    private  int id;
+    public String name;
+    public String type;
+    public int id;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
