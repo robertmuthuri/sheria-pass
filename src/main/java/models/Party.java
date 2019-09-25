0d49cd44826a6da129cb7247437210900dfe6297
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-25 16:11:11 +0300

    Create class Party and constructor

---
+++ src/main/java/models/Party.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Party {
+    private  String name;
+    private String type;
+    private  int id;
+
+    public Party(String name, String type, int id) {
+        this.name = name;
+        this.type = type;
+        this.id = id;
+    }
+}
