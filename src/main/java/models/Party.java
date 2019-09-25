30d98eca7710fef45cc0608894f35fda7ece6f6e
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-25 16:26:58 +0300

    Add setters and getters

--- src/main/java/models/Party.java
+++ src/main/java/models/Party.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.Objects;
+
 public class Party {
     private  String name;
     private String type;
@@ -9,5 +11,44 @@ public class Party {
         this.name = name;
         this.type = type;
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Party party = (Party) o;
+        return id == party.id &&
+                Objects.equals(name, party.name) &&
+                Objects.equals(type, party.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, type, id);
     }
 }
