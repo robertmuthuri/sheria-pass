13b31e2c1a168994c9a1edbf12ca8ec433455c7b
Robert Muthuri <muthuri.r@gmail.com>
2019-09-26 14:45:23 +0300

    Adds revised db schema to the project.

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,5 +1,21 @@
 SET MODE PostgreSQL;
 
+CREATE TABLE IF NOT EXISTS laws (
+id int PRIMARY KEY auto_increment,
+law_citation VARCHAR,
+law_summary VARCHAR,
+law_url VARCHAR
+);
+
+CREATE TABLE IF NOT EXISTS caselaws (
+ id int PRIMARY KEY auto_increment,
+ case_citation VARCHAR,
+ case_summary VARCHAR,
+ case_court VARCHAR,
+ case_holding VARCHAR,
+ case_url VARCHAR
+ );
+
 CREATE TABLE IF NOT EXISTS parties (
  id int PRIMARY KEY auto_increment,
  name VARCHAR,
@@ -8,6 +24,17 @@ CREATE TABLE IF NOT EXISTS parties (
 
  CREATE TABLE IF NOT EXISTS judges (
  id int PRIMARY KEY auto_increment,
- name VARCHAR,
+ judge_name VARCHAR,
+ judge_rank VARCHAR
  );
 
+CREATE TABLE IF NOT EXISTS caselaws_parties (
+ id int PRIMARY KEY auto_increment,
+ case_id VARCHAR,
+ party_id VARCHAR
+ );
+CREATE TABLE IF NOT EXISTS caselaws_judges (
+ id int PRIMARY KEY auto_increment,
+ case_id VARCHAR,
+ judge_id VARCHAR
+ );
