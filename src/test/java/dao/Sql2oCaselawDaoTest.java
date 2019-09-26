218f98db1a7c5e54c93532d767fa386ce56f2c65
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-26 22:42:44 +0300

    Update method find by id

--- src/test/java/dao/Sql2oCaselawDaoTest.java
+++ src/test/java/dao/Sql2oCaselawDaoTest.java
@@ -29,4 +29,3 @@ public class Sql2oCaselawDaoTest {
         return new CaseLaw("Raila Amolo Odinga & another v Independent Electoral and Boundaries Commission 2 others [2017] eKLR", "Validity of a presidential election-petition challenging the validity of the president elect-allegations of non-compliance with the Constitution and electoral laws - allegations of various  irregularities and illegalities during the conduct of the elections","Supreme Court of Kenya","(i) A declaration is hereby issued that the Presidential Election held on 8th August, 2017 was not conducted in accordance with the Constitution and the applicable law rendering the declared result invalid, null and void;","http://www.kenyalaw.org/kl/fileadmin/pdfdownloads/2017ElectionPetition/Presidential_Petition_1_of_2017.pdf"); }
     }
 
-}
--- src/test/java/dao/Sql2oJudgeDaoTest.java
+++ src/test/java/dao/Sql2oJudgeDaoTest.java
@@ -1,23 +1,34 @@
 package dao;
 
+import models.Judge;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
 
 import static org.junit.Assert.*;
 
 public class Sql2oJudgeDaoTest {
-
+    private Connection conn;
+    private Sql2oJudgeDao judgeDao;
     @Before
     public void setUp() throws Exception {
-    }
 
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        judgeDao = new Sql2oJudgeDao(sql2o);
+                conn = sql2o.open();
+
+    }
     @After
     public void tearDown() throws Exception {
+        conn.close();
     }
 
     @Test
     public void add() {
+
     }
 
     @Test
@@ -42,5 +53,10 @@ public class Sql2oJudgeDaoTest {
 
     @Test
     public void clearAll() {
+    }
+
+    public Judge setUpNewJudge(){
+        Judge judge = new Judge("");
+        return judge;
     }
 }
