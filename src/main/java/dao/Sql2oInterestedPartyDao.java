1fcec930a1db8adb9ca131e12dcfd6f18b1a5a7b
daisyarusey <daisyarusey.dc@gmail.com>
2019-09-26 22:00:54 +0300

    Update delete by id method

--- src/main/java/dao/Sql2oInterestedPartyDao.java
+++ src/main/java/dao/Sql2oInterestedPartyDao.java
@@ -8,6 +8,7 @@ import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 import org.sql2o.Sql2oException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Sql2oInterestedPartyDao implements InterestedPartyDao{
@@ -67,16 +68,38 @@ public class Sql2oInterestedPartyDao implements InterestedPartyDao{
     }
 
     @Override
-    public List<CaseLaw> getCaseByPetitionerId() {
-        return null;
+    public List<CaseLaw> getCaseLawsForInterestedParty( int party_id) {
+        List<CaseLaw> caseLaws = new ArrayList();
+        String joinQuery = "SELECT case_id FROM caselaws_parties WHERE party_id = :party_id";
+
+        try (Connection con = sql2o.open()) {
+            List<Integer> allCaseLawIds = con.createQuery(joinQuery)
+                    .addParameter("party_id",party_id)
+                    .executeAndFetch(Integer.class);
+            for (Integer case_id : allCaseLawIds){
+                String caseLawQuery = "SELECT * FROM caselaws WHERE id = :case_id";
+                caseLaws.add(
+                        con.createQuery(caseLawQuery)
+                                .addParameter("case_id", case_id)
+                                .executeAndFetchFirst(CaseLaw.class));
+            }
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+        return caseLaws;
     }
+
 
     @Override
     public void deleteById(int id) {
         String sql = "DELETE from parties WHERE id = :id";
+        String deleteJoin = "DELETE from caselaws_parties WHERE party_id = :party_id";
         try (Connection con = sql2o.open()) {
             con.createQuery(sql)
                     .addParameter("id", id)
+                    .executeUpdate();
+            con.createQuery(sql)
+                    .addParameter("party_id", id)
                     .executeUpdate();
         } catch (Sql2oException ex){
             System.out.println(ex);
